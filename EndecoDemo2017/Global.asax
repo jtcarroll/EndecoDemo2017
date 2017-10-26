a4d55baadd39cccea860be01a5e7c4cc82d162f9
John Carroll <John Carroll>
2017-10-26 14:20:59 +0100

    Add project files.

---
+++ EndecoDemo2017/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EndecoDemo2017.MvcApplication" Language="C#" %>
---
+++ EndecoDemo2017/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EndecoDemo2017.Startup))]
+namespace EndecoDemo2017
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
